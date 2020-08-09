2ee97c6823feeaa04fd1266aca3ce96c853d7bea
Phun <phunphun484@gmail.com>
2020-08-09 21:16:26 +0700

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+This package uses the Thai Font Adjustor package which developed and copyrighted by SaladLab. 
+
+The original repository :
+https://github.com/SaladbowlCreative/Unity3D.ThaiFontAdjuster/releases
---
+++ Scripts/ThaiText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteAlways]
+public class ThaiText : Text
+{
+    private string oldText;
+    private void FixThaiText()
+    {
+        text = ThaiFontAdjuster.Adjust(text);
+    }
+
+    void Awake()
+    {
+        oldText = text;
+    }
+
+    void Update()
+    {
+        if (!oldText.Equals(text))
+        {
+            FixThaiText();
+            oldText = text;
+        }
+    }
+
+
+}
